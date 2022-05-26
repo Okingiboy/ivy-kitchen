2939fd00c6e47f10167ba58778cfb9f5ecb7b635
“jameschukwu“ <okoriechukwu@yahoo.com>
2022-05-26 11:02:10 -0500

    updated files

--- assets/Js/placeholder.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector('button');
-const tooltip = document.querySelector('#tooltip');
-btn.addEventListener('mouseover', function() {
-  tooltip.style.display = 'block';
-});
-btn.addEventListener('mouseout', function() {
-  tooltip.style.display = 'none';
-});
